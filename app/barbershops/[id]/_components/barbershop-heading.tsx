5de85b663b12d0386bc5ce0f9ede03f5b886a0f0
finotellicarlos <carlos.finotelli@outlook.com>
2024-02-22 16:08:55 -0300

    refact: create component barbersop-heading

---
+++ app/barbershops/[id]/_components/barbershop-heading.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import { Barbershop } from "@prisma/client";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface BarbershopHeadingProps {
+  barbershop: Barbershop;
+}
+
+const BarbershopHeading = ({ barbershop }: BarbershopHeadingProps) => {
+  const router = useRouter();
+
+  const handleBackToHomePage = () => {
+    router.back();
+  };
+
+  return (
+    <div className="">
+      <div className="h-[250px] w-full relative">
+        <Button
+          variant="outline"
+          size="icon"
+          className="z-50 top-2 left-2 absolute"
+          onClick={handleBackToHomePage}
+        >
+          <ChevronLeftIcon size={16} />
+        </Button>
+
+        <Button
+          variant="outline"
+          size="icon"
+          className="z-50 top-2 right-2 absolute"
+        >
+          <MenuIcon size={16} />
+        </Button>
+
+        <Image
+          fill
+          src={barbershop.imageUrl}
+          alt={barbershop.name}
+          className="object-cover opacity-85"
+        />
+      </div>
+
+      <div className="pt-3 px-3 pb-6 border-solid border-b border-secondary space-y-2">
+        <h1 className="font-bold text-xl">{barbershop.name}</h1>
+        <div className="flex items-center gap-1">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop.address}</p>
+        </div>
+        <div className="flex items-center gap-1">
+          <StarIcon className="text-primary" size={18} />
+          <p className="text-sm">4.7 (754 avaliações)</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BarbershopHeading;
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,7 +1,5 @@
-import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
-import Image from "next/image";
+import BarbershopHeading from "./_components/barbershop-heading";
 
 interface BarbershopDetailsPageParams {
   params: {
@@ -26,46 +24,7 @@ const BarbershopDetailsPage = async ({
     return null;
   }
 
-  return (
-    <div className="">
-      <div className="h-[250px] w-full relative">
-        <Button
-          variant="outline"
-          size="icon"
-          className="z-50 top-2 left-2 absolute"
-        >
-          <ChevronLeftIcon size={16} />
-        </Button>
-
-        <Button
-          variant="outline"
-          size="icon"
-          className="z-50 top-2 right-2 absolute"
-        >
-          <MenuIcon size={16} />
-        </Button>
-
-        <Image
-          fill
-          src={barbershop.imageUrl}
-          alt={barbershop.name}
-          className="object-cover opacity-85"
-        />
-      </div>
-
-      <div className="pt-3 px-3 pb-6 border-solid border-b border-secondary space-y-2">
-        <h1 className="font-bold text-xl">{barbershop.name}</h1>
-        <div className="flex items-center gap-1">
-          <MapPinIcon className="text-primary" size={18} />
-          <p className="text-sm">{barbershop.address}</p>
-        </div>
-        <div className="flex items-center gap-1">
-          <StarIcon className="text-primary" size={18} />
-          <p className="text-sm">4.7 (754 avaliações)</p>
-        </div>
-      </div>
-    </div>
-  );
+  return <BarbershopHeading barbershop={barbershop} />;
 };
 
 export default BarbershopDetailsPage;
