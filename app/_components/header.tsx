096a7881ae11c7bdf13364ee44f7c13f445cedc3
finotellicarlos <carlos.finotelli@outlook.com>
2024-02-28 15:38:32 -0300

    feat: add link to logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,5 +1,6 @@
 import { MenuIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import SideMenu from "./side-menu";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
@@ -9,13 +10,15 @@ const Header = () => {
   return (
     <Card>
       <CardContent className="flex justify-between flex-row items-center mx-auto p-5">
-        <Image
-          src="/logo.png"
-          alt="Barber Shop Logo"
-          height={22}
-          width={120}
-          loading="eager"
-        />
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Barber Shop Logo"
+            height={22}
+            width={120}
+            loading="eager"
+          />
+        </Link>
 
         <Sheet>
           <SheetTrigger asChild>
